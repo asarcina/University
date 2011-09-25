d4a12919a352e9d2a33367e836b19c772b6ddc9c
Alessandro Sarcina <a.sarcina@gmail.com>
2011-09-26 00:09:56 +0200

    versione 2

--- DitanceVector/node.rb
+++ DitanceVector/node.rb
@@ -4,8 +4,10 @@ class Nodes
   #hash DV incicizzato sui nomi dei nodi
   # indirizzo del nodo
   #Tabella instradamento
-  attr_accessor :address , :rtable , :dist_vec
   
+  #il nodo deve anche aprire una socket server e una client appena inizializzato
+  attr_accessor :address , :rtable , :dist_vec , :port
+    @port=10000
   def initialize(node_address,links)
     #inizializzo il nome del nodo
     #inizializzo la tabella di instradamento a 0
@@ -13,14 +15,27 @@ class Nodes
     #inizializzo il DV con i vicini
     @address = node_address
     @rtable=RoutingTable.new links
-    @dist_vec=@rtable.getDistanceVector
-#    puts @rtable.table
+    @dist_vec=@rtable.getDistanceVector 
+    @port=@port+node_address.to_i
+   puts @rtable.table
 #    puts @address
 #    puts "----------"
 #    puts @dist_vec
 #    
+
   end
-  #@rtable.each{|r| puts r}
+  
+  def start_server
+    server=TCPServer.open(@port)
+    loop{
+      Thread.start(server.accept) do |client|
+        #--------------
+        #funzione da eseguire sul client
+        #invio dv
+        client.close
+        end
+    }
+  end
   
   
 end
--- DitanceVector/start.rb
+++ DitanceVector/start.rb
@@ -1,2 +1,32 @@
 require 'node'
-c=Nodes.new 1 , [[2,4],[4,1]] 
+
+#ogni nodo mi fa partire un thread che dovrà simulare  un nodo indipendente su una macchina distinta
+threads=[]
+nodes=[]
+#n1=Nodes.new 1 , [[2,1],[4,2],[6,9]] 
+#n2=Nodes.new 1 , [[1,4],[3,1]] 
+#n3=Nodes.new 1 , [[2,1],[3,1]] 
+#n4=Nodes.new 1 , [[1,4],[4,1]] 
+#n1.rtable.table
+#n2.rtable.table
+#n3.rtable.table
+#n4.rtable.table
+
+params=[
+{:address=> 1, :links=>[[2,1],[4,2],[6,9]]},
+{:address=> 2, :links=>[[1,4],[3,1]]},
+{:address=> 3, :links=>[[2,1],[3,1]]},
+{:address=> 4, :links=>[[1,4],[4,1]]}
+
+]
+params.each do |p|
+ 
+  threads << Thread.new do
+    nodes << Nodes.new(p[:address] , p[:links]) 
+    puts "nodo creato" + p[:address].to_s
+    end
+
+ end
+ 
+ 
+# threads.each{|t| t.join}
